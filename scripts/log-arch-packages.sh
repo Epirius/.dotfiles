81ec2a2d89816d90afc6ce47b8e1d89681169ff8
Felix Kaasa <mrKaasa@protonmail.com>
2023-11-25 20:16:30 +0100

    updating scripts to log packages

--- scripts/log-arch-packages.sh
+++ scripts/log-arch-packages.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-pacman -Qqem > ~/.dotfiles/packages/arch-aur
-pacman -Qqen | rg '^ttf-|^otf-' > ~/.dotfiles/packages/arch-fonts
-pacman -Qqen | rg -v '^ttf-|^otf-' > ~/.dotfiles/packages/arch-core
+su - fk -c "pacman -Qqem > /home/fk/.dotfiles/packages/arch-aur"
+su - fk -c "pacman -Qqen | rg '^ttf-|^otf-' > /home/fk/.dotfiles/packages/arch-fonts"
+su - fk -c "pacman -Qqen | rg -v '^ttf-|^otf-' > /home/fk/.dotfiles/packages/arch-core"
 
+BASE="/home/fk/.dotfiles/"
+cd $BASE
+cd $BASE && git add packages/.
+su - fk -c "cd $BASE && git commit -m '[AUTO] updated packages'"
+su - fk -c "cd $BASE && git push"
+
+# TODO auto commit the 3 files after running
